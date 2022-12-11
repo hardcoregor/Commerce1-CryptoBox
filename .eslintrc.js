a03c5a3f6809664741432bd01730f2fd51250ea0
hardcoregor <spraytown2014@gmail.com>
2022-12-12 01:51:13 +0200

    some problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,8 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
+  'extends': [
+    'plugin:react/recommended'
   ],
   parserOptions: {
     ecmaFeatures: {
