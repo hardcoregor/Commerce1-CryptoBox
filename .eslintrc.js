9c950b2435baf0a62d00b35239f485663f025a00
hardcoregor <spraytown2014@gmail.com>
2022-12-12 02:02:59 +0200

    done

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  'extends': [
-    'plugin:react/recommended'
-  ],
+  'extends': "next",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
