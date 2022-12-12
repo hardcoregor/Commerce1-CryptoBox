ac0deab6dbe14eba578d000c814f85d85201278c
hardcoregor <spraytown2014@gmail.com>
2022-12-12 22:41:43 +0200

    add background mouse move parallax effect

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,24 @@
 import { ThemeProvider } from 'next-themes';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Footer, Navbar } from '../components';
 
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
+  const root = useMemo(() => document.querySelector(":root"));
+
   const [showModal, setShowModal] = useState(true);
+  const parallaxHandler = (e) => {
+    const x = (e.clientX - window.innerWidth / 2) / 5;
+    const y = (e.clientY - window.innerHeight / 2) / 5;
+
+    root.style.setProperty('--posX', -x);
+    root.style.setProperty('--posY', -y);
+
+  }
 
   return (
-    <div className="parallax h-full overflow-y-hidden flex-1 flex flex-col">
+    <div className="parallax h-full overflow-hidden flex-1 flex flex-col" onMouseMove={parallaxHandler}>
       <div className="bege" />
       <div className="parallaxBG" />
       <ThemeProvider>
